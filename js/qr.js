0efd06f5c59311eb58451962c3071d51e2b4edf2
Marc Riera <mrc2407@gmail.com>
2014-02-05 12:28:16 +0100

    Test

--- js/qr.js
+++ js/qr.js
@@ -1,6 +1,10 @@
 function read(a)
 {
-    $("#qr-value").text(a);
+    // $("#qr-value").text(a);
+  $.getJSON('http://192.168.1.96:3000/conferences/futurejs?id=' + a, function(data){
+    // $('#output').html(data.contents);
+    alert(data.contents);
+  });
 }
-    
+
 qrcode.callback = read;
