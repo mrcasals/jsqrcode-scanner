11f039c5d53c35225f55fca166e617be6794faf7
Marc Riera <mrc2407@gmail.com>
2014-02-05 12:37:57 +0100

    Debug

--- js/qr.js
+++ js/qr.js
@@ -3,6 +3,7 @@ function read(a)
     // $("#qr-value").text(a);
   $.getJSON('http://192.168.1.96:3000/conferences/futurejs/tickets?id=' + a, function(data){
     // $('#output').html(data.contents);
+    console.log(data)
     alert(data.contents);
   });
 }
