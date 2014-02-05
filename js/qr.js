7317c503b28af636a1e06aba1d78d695e0e26456
Marc Riera <mrc2407@gmail.com>
2014-02-05 12:33:51 +0100

    Fix url

--- js/qr.js
+++ js/qr.js
@@ -1,7 +1,7 @@
 function read(a)
 {
     // $("#qr-value").text(a);
-  $.getJSON('http://192.168.1.96:3000/conferences/futurejs?id=' + a, function(data){
+  $.getJSON('http://192.168.1.96:3000/conferences/futurejs/tickets?id=' + a, function(data){
     // $('#output').html(data.contents);
     alert(data.contents);
   });
