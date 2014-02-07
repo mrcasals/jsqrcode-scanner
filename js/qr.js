b89d082faa1b649a1943ed5faf8ca90f785fab06
Marc Riera <mrc2407@gmail.com>
2014-02-07 09:56:55 +0100

    Tezt

--- js/qr.js
+++ js/qr.js
@@ -1,11 +1,16 @@
 function read(a)
 {
     // $("#qr-value").text(a);
-  $.getJSON('http://192.168.1.96:3000/conferences/futurejs/tickets?id=' + a, function(data){
-    // $('#output').html(data.contents);
-    console.log(data)
-    alert(data.contents);
-  });
+  // $.getJSON('http://192.168.1.96:3000/conferences/futurejs/tickets?id=' + a, function(data){
+  //   // $('#output').html(data.contents);
+  //   console.log(data)
+  //   alert(data.contents);
+  // });
+
+  $.post('http://192.168.1.96:3000/conferences/futurejs/tickets', {id: a}, function(data, textStatus){
+    console.log(data);
+    console.log(textStatus)
+  }, 'json')
 }
 
 qrcode.callback = read;
